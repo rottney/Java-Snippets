0be0798357a6c98f6cd9ad25ac2cd6a574440087
Ryan Ottney <rottney123@gmail.com>
2019-01-02 23:34:01 -0800

    Update Subarray.java

--- Subarray.java
+++ Subarray.java
@@ -1,22 +1,23 @@
 /*
   We define the following:
-
-  A subarray of an -element array is an array composed from a contiguous block of the original array's elements. For example, if , then the subarrays are , , , , , and . Something like  would not be a subarray as it's not a contiguous subsection of the original array.
-  The sum of an array is the total sum of its elements.
-  An array's sum is negative if the total sum of its elements is negative.
-  An array's sum is positive if the total sum of its elements is positive.
-  Given an array of  integers, find and print its number of negative subarrays on a new line.
+    A subarray of an n-element array is an array composed from a contiguous block of the original array's elements.
+    For example, if A = [1, 2, 3], then the subarrays are [1], [2], [3], [1, 2], [2, 3], and [1, 2, 3].
+    Something like [1, 3] would not be a subarray as it's not a contiguous subsection of the original array.
+    The sum of an array is the total sum of its elements.
+    An array's sum is negative if the total sum of its elements is negative.
+    An array's sum is positive if the total sum of its elements is positive.
+    Given an array of n integers, find and print its number of negative subarrays on a new line.
 
   Input Format
-
-  The first line contains a single integer, , denoting the length of array . 
-  The second line contains  space-separated integers describing each respective element, , in array .
+    The first line contains a single integer, n, denoting the length of array A. 
+    The second line contains n space-separated integers describing each respective element, A[i], in array A.
 
   Constraints
-
+    1 <= n <= 100
+    -10^4 <= A[i] <= 10^4
+  
   Output Format
-
-  Print the number of subarrays of  having negative sums.
+    Print the number of subarrays of A having negative sums.
 */
 
 import java.io.*;
