977947b15c14081cec527f27ce43ec10899f1982
Ryan Ottney <rottney123@gmail.com>
2019-01-08 10:52:23 -0800

    Update ColumnToRow.java

--- ColumnToRow.java
+++ ColumnToRow.java
@@ -3,6 +3,7 @@ import java.util.*;
 /*
     Transposes a column into a row
     Removes quotation (") characters, if present
+    Prints comma (,) character in between elements
 */
 
 public class ColumnToRow {
@@ -13,13 +14,18 @@ public class ColumnToRow {
         Scanner scan = new Scanner(System.in);
         
         while (scan.hasNextLine()) {
-            vect.add(scan.nextLine());
+            if (scan.nextLine().compareTo("") != 0) {
+                vect.add(scan.nextLine());
+            }
         }
         scan.close();
         
         // Logic
         for (int i = 0; i < vect.size(); i++) {
-            System.out.print(vect.get(i).replace("\"", "") + ' ');
+            System.out.print(vect.get(i).replace("\"", ""));
+            if (i < vect.size() - 1) {
+                System.out.print(", ");
+            }
         }
     }
 }
