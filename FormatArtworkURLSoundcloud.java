0d1396ade41f8a835ff907ca891f05696bd0080a
Ryan Ottney <rottney123@gmail.com>
2019-01-13 17:25:04 -0800

    Create FormatArtworkURLSoundcloud.java
    Input:  Copy Attribute from SoundCloud image using Safari inspector
    Output:  A URL you can use to download image

---
+++ FormatArtworkURLSoundcloud.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+/*
+    Input:  Copy Attribute from SoundCloud image
+        using Safari inspector
+    Output:  A URL you can use to download image
+*/
+public class FormatArtworkURLSoundcloud{
+    
+    public static void main(String []args){
+        Character ch = '"';
+        String q = ch.toString();
+        Scanner scan = new Scanner(System.in);
+        System.out.println(scan.nextLine()
+            .replace("style="
+            .concat(q)
+            .concat("background-image: url(\\")
+            .concat(q), "")
+            .replace("\\"
+            .concat(q)
+            .concat("); width: 500px; height: 500px; opacity: 1;")
+            .concat(q), ""));
+            scan.close();
+    }
+}
