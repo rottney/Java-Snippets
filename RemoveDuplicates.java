e914ec92aa86d52349b77280314fd89ca341759b
Ryan Ottney <rottney123@gmail.com>
2019-08-18 14:09:07 -0700

    Create RemoveDuplicates.java

---
+++ RemoveDuplicates.java
@@ -0,0 +1,43 @@
+import java.util.*;
+
+/*
+    Removes duplicate strings from input
+*/
+
+public class RemoveDuplicates {
+
+    public static void main(String []args) {
+        // Store input
+        Vector<String> input = new Vector<String>();
+        Scanner scan = new Scanner(System.in);
+
+        while (scan.hasNextLine()) {
+            if (scan.nextLine().compareTo("") != 0) {
+                input.add(scan.nextLine());
+            }
+        }
+        scan.close();
+
+        // Build output list
+        Vector<String> output = new Vector<String>();
+        output.add(input.get(0));
+        
+        for (int i = 1; i < input.size(); i++) {
+            boolean isDup = false;
+            for (int j = 0; j < output.size(); j++) {
+                if (input.get(i).compareTo(output.get(j)) == 0) {
+                    isDup = true;
+                    break;
+                }
+            }
+            if (!isDup) {
+                output.add(input.get(i));
+            }
+        }
+        
+        // Print output list
+        for (int i = 0; i < output.size(); i++) {
+            System.out.println(output.get(i));
+        }
+    }
+}
